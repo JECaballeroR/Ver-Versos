fec36e00d638e338ae4a09e78bb67625db926b58
Jorge Esteban Caballero Rodríguez <62854093+JECaballeroR@users.noreply.github.com>
2021-02-08 20:37:47 -0500

    Update interface.py

--- interface.py
+++ interface.py
@@ -10,7 +10,7 @@ import os
 # Typically we run this on the same machine
 #api_url = "http://127.0.0.1:8000/infer"
 api_port=os.environ.get('PORT')
-api_url = "0.0.0.0:"+str(api_port)+"/infer"
+api_url = "http://0.0.0.0:"+str(api_port)+"/infer"
 
 cache_dest = Path("data/streamlit_image_cache")
 cache_dest.mkdir(exist_ok=True, parents=True)
